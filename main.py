e00d369e2c395a8b5dbc5d341ea96cd515c89ffd
Amanullah Tariq <amanullah.tariq@gmail.com>
2018-07-19 11:46:04 +0200

    image blurring is added but blurring is not smooth

--- main.py
+++ main.py
@@ -3,36 +3,61 @@ import cv2
 import numpy as np
 from model import Deeplabv3
 import os
-
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# deeplab_model = Deeplabv3()
+def start_video():
+    cap = cv2.VideoCapture(1)
+    while True:
+        _, frame = cap.read()
 
-img = plt.imread("imgs//image4.jpg")
-w,h, _ = img.shape
-deeplab_model = Deeplabv3()
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
 
-
-ratio = 512. / np.max([w,h])
-
-resized = cv2.resize(img,(int(ratio*h),int(ratio*w)))
-resized = resized / 127.5 - 1
-pad_x = int(512 - resized.shape[0])
-resized2 = np.pad(resized,((0,pad_x),(0,0),(0,0)),mode='constant')
-
-res = deeplab_model.predict(np.expand_dims(resized2,0))
-labels = np.argmax(res.squeeze(),-1)
-labels = labels[:-pad_x]
+    cv2.destroyAllWindows()
+    cap.release()
 
 
+def blur_image(image_path):
+    img = plt.imread(image_path)
+    w, h, _ = img.shape
+    deeplab_model = Deeplabv3(backbone='xception', OS=8)
 
-mask = labels == 0
+    ratio = 512. / np.max([w, h])
 
-resizedFrame = cv2.resize(img, (labels.shape[1],labels.shape[0]))
-blur = cv2.GaussianBlur(resizedFrame, (41,41), 0)
-resizedFrame[mask] = blur[mask]
-plt.imshow(resizedFrame)
-plt.waitforbuttonpress()
+    resized = cv2.resize(img, (int(ratio * h), int(ratio * w)))
+    resized = resized / 127.5 - 1
+    pad_x = int(512 - resized.shape[0])
+    resized2 = np.pad(resized, ((0, pad_x), (0, 0), (0, 0)), mode='constant')
+
+    res = deeplab_model.predict(np.expand_dims(resized2, 0))
+    labels = np.argmax(res.squeeze(), -1)
+    labels = labels[:-pad_x]
+
+    mask = labels == 0
+
+    resizedFrame = cv2.resize(img, (labels.shape[1], labels.shape[0]))
+    blur = cv2.GaussianBlur(resizedFrame, (15, 15), 0)
+    resizedFrame[mask] = blur[mask]
+    plt.imshow(resizedFrame)
+    plt.waitforbuttonpress()
+
+
+def test_blurring():
+    img = cv2.imread("imgs//image1.jpg")
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow( "HSV Image", hsv)
+
+
+
+if __name__== "__main__":
+    #blur_image("imgs//image1.jpg")
+    test_blurring()
+
+
+
+
+
 
 
